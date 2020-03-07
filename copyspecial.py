9b707ac4822031078ec4ce3a9f28b01d17746ff6
Justin Miller <indymiller@gmail.com>
2020-03-07 18:47:09 -0500

    get_special and copy_to complete

--- copyspecial.py
+++ copyspecial.py
@@ -16,19 +16,47 @@ import subprocess
 import argparse
 
 # This is to help coaches and graders identify student assignments
-__author__ = "???"
+__author__ = 'Justin Miller'
 
 
 # +++your code here+++
 # Write functions and modify main() to call them
 
-def main():
+get_special_paths(d):
+    """returns a list of the absolute paths of the special files in the given directory"""
+    path_list = []
+    all_dir_paths = os.listdir(d)
+    for each in all_dir_paths:
+        match = re.search(r'__w__', each)
+        if match:
+            path_list.append(os.path.abspath(os.path.join(d, each)))
+    print(path_list)
+    return path_list
+
+
+copy_to(paths, to_dir) 
+    """given a list of paths, copies those files into the given directory"""
+    if not os.path.exists(to_dir):
+        os.mkdir(to_dir)
+    for path in paths:
+        file = os.path.basename(path)
+        shutil.copy(path, os.path.join(to_dir, file))
+
+zip_to(paths, zippath) 
+    """given a list of paths, zip those files up into the given zipfile"""
+
+get_args():
     # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO need an argument to pick up 'from_dir'
     args = parser.parse_args()
+    return args
+
+def main():
+    args = get_args()
+
 
     # TODO you must write your own code to get the cmdline args.
     # Read the docs and examples for the argparse module about how to do this.
